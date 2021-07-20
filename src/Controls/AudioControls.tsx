88a79bb6aafa9ca181743c4b6d7b4410c37520a1
surya darma <budi.surya@kumparan.com>
2021-07-20 14:00:38 +0800

    fix audio volume on click

--- src/Controls/AudioControls.tsx
+++ src/Controls/AudioControls.tsx
@@ -7,7 +7,7 @@ import VolumeFull from '../../assets/volume-full.svg'
 import VolumeMute from '../../assets/volume-mute.svg'
 
 const getVolumeIcons = (volume: number): string => {
-  if (volume === 1 && volume >= 0.75) {
+  if (volume <= 1 && volume >= 0.75) {
     return VolumeFull
   }
   if (volume < 0.75 && volume >= 0.5) {
@@ -25,6 +25,7 @@ const getVolumeIcons = (volume: number): string => {
 const AudioControls = () => {
   // used to check the current volume before it was toggled to mute or not
   const currentVolumeRef = React.useRef(1)
+  const volumeTrackRef = React.useRef<HTMLDivElement>(null)
   const {player, volume} = useProjector()
   const onTogglemute = React.useCallback(() => {
     if (player) {
@@ -36,13 +37,42 @@ const AudioControls = () => {
       player.volume = mute ? currentVolumeRef.current : 0
     }
   }, [player, volume])
+
+  const onClickTrack = React.useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      if (volumeTrackRef.current && player) {
+        const track = volumeTrackRef.current
+        const trackWidth = track.clientWidth
+        // compat: positioning from click
+        const clickFromOffset = event.pageX - (track.offsetLeft + 20)
+
+        const clickPosition =
+          clickFromOffset > trackWidth ? trackWidth : clickFromOffset
+
+        const volumePersentation = clickPosition / 100
+
+        player.volume = volumePersentation
+      }
+    },
+    [player]
+  )
+
   return (
     <div className="pcontrol-audio">
       <button onClick={onTogglemute}>
         <img src={getVolumeIcons(volume)} />
       </button>
       <div className="pcontrol-audio__volume-range">
-        <input type="range" min={0} max={100} />
+        <div
+          className="pcontrol-audio__volume-track"
+          ref={volumeTrackRef}
+          onClick={onClickTrack}
+        >
+          <div
+            className="pcontrol-audio__volume-progress"
+            style={{width: `${volume * 100}%`}}
+          />
+        </div>
       </div>
     </div>
   )
